95e42116d098259d5365866111859b664631bf29
melonik97 <woj.seba02@gmail.com>
2023-06-21 11:14:56 +0200

    Dodanie cw 3 do repozytorium

--- README.md
+++
@@ -1,9 +0,0 @@
-Instalacja i działanie programu:
-
-`make install` - Instaluje wszystkie wymagane moduły python do działania programu
-
-`make run` - Uruchamia aplikacje flask na porcie 5000 pod wszystkimi dostepnymi adresami (--host=0.0.0.0 dodane dla kompatybilności z WSL)
-
-`make pylint` - Uruchamia test jakości kodu
-
-`make clean` - Usuwa pliki cache
--- app.py
+++ app.py
@@ -1,14 +1,9 @@
-from flask import Flask, render_template
-from markupsafe import escape
-app = Flask(__name__)
+from flask import Flask 
+app = Flask(__name__) 
 
+@app.route('/') 
+def index(): 
+    return '<h1>Hello WSB! Greetings from Flask & Docker!</h1>' 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name="Sebastian")
+if __name__ == "__main__": 
+    app.run(debug=True)
